9b622d5892d8c2d0991f2eda0b8e9527673a0b22
sams99999 <samuelgoma184@gmail.com>
2024-09-18 22:07:04 +0200

    multiplicacio feta

--- src/calc.c
+++ src/calc.c
@@ -3,9 +3,9 @@
 #include "calc.h"
 
 double multiplicacio(double a, double b){
-    double c = 0;
+    double d = 0;
     for(int i = 0; i < a; i++){
-        c += b;
+        d += b;
     }
-    return c;
+    return d;
 }
