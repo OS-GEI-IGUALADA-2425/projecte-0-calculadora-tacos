0af3db29d8cb6cb5b076d1b6970c3f261f5c1f58
guillem <guillem@lab00-debian.local>
2024-09-18 19:20:13 +0200

    @feat: Implementació de la funció de suma

--- src/calc.c
+++ src/calc.c
@@ -1,3 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include "calc.h"
+
+double suma(int a, int b) {
+    return a + b;
+}
--- src/calc.h
+++ src/calc.h
@@ -1,5 +1,6 @@
 #ifndef CALC_H
 #define CALC_H
 
+double suma(int a, int b);
 
 #endif // CALC_H
