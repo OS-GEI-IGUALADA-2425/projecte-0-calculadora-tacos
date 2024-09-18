28a5d9c08257bef933b4e281c7cc57a1331ca5ce
sams99 <sams99@am-080027c2b014.intern>
2024-09-18 19:25:28 +0200

    @feat: Implementació de la funció de suma

--- src/calc.c
+++ src/calc.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 #include "calc.h"
+
+double multiplicacio(double a, double b){
+    double c = 0;
+    for(int i = 0; i < a; i++){
+        c += b;
+    }
+    return c;
+}
--- src/calc.h
+++ src/calc.h
@@ -1,5 +1,5 @@
 #ifndef CALC_H
 #define CALC_H
 
-
+double multiplicacio(double a, double b);
 #endif // CALC_H
